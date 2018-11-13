70c8cf46064c79a41230932720c9d08565288ee1
Teodoro Gomez LLanos Juarez <tgomez8888@gmail.com>
2018-11-12 18:42:45 -0800

    Add changes to main component

--- src/modules/booking/components/Booking/index.jsx
+++ src/modules/booking/components/Booking/index.jsx
@@ -1,45 +1,43 @@
 import React, { Component } from "react";
-import {  
-  either,  
-  ifElse,
-  identity,  
-  isEmpty,
-  isNil,
-} from "ramda";
-import {defaultRooms, processRoomSelectionChanges, updateRoom} from "../../services/booking";
+import { either, equals, ifElse, identity, isEmpty, isNil } from "ramda";
+import {
+  defaultRooms,
+  processRoomSelectionChanges,
+  updateRoom
+} from "../../services/booking";
 import Room from "./Room";
 import Actions from "./Actions";
 import "../../styles/booking.scss";
 
 export class Booking extends Component {
   constructor(props) {
-    super(props);   
+    super(props);
 
     this.state = {
-      rooms: ifElse(
-        either(isNil, isEmpty),
-        () => defaultRooms,
-        identity
-      )(props.rooms)
+      rooms: ifElse(either(isNil, isEmpty), () => defaultRooms, identity)(
+        props.rooms
+      )
     };
   }
 
-  componentWillReceiveProps(newProps) {    
+  componentWillReceiveProps(newProps) {
     // I would expect an HOC that wraps this component and calls a GraphQL Query to get the rooms
     // and pass the rooms value as a prop either initially or as a load function. This
     // will be for load function, constructor is for initial prop load
-    
-    // if (newProps.rooms && !isEmpty(difference(this.props.rooms))   this.props.rooms !== newProps.rooms) {
-    //   this.setState({ rooms: newProps.rooms });
-    // }
+
+    if (newProps.rooms && !equals(newProps.rooms, this.props.rooms)) {
+      this.setState({ rooms: newProps.rooms });
+    }
   }
 
   handleRoomValueChanges = newRoom => {
-    this.setState({ rooms: updateRoom(newRoom, this.state.rooms)});
-  }
+    this.setState({ rooms: updateRoom(newRoom, this.state.rooms) });
+  };
 
   handleRoomSelectionChanges = newRoom => {
-    this.setState({ rooms: processRoomSelectionChanges(newRoom, this.state.rooms)});
+    this.setState({
+      rooms: processRoomSelectionChanges(newRoom, this.state.rooms)
+    });
   };
 
   handleSubmit = e => {
@@ -63,7 +61,7 @@ export class Booking extends Component {
                 onRoomSelectionChange={this.handleRoomSelectionChanges}
               />
             ))}
-          <Actions />          
+          <Actions />
         </form>
       </main>
     );
