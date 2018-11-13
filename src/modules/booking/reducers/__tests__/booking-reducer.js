61a2a9d6e197a0e46df544982aa1ebc1084a2732
Teodoro Gomez LLanos Juarez <tgomez8888@gmail.com>
2018-11-13 06:29:45 -0800

    Add reducer tests

--- src/modules/booking/reducers/__tests__/booking-reducer.js
+++ src/modules/booking/reducers/__tests__/booking-reducer.js
@@ -1,4 +1,3 @@
-import { defaultRooms } from "../../services/booking";
 import { indexBy, prop } from "ramda";
 import { getRooms, getRoomsId, getRoomById } from "../../selectors";
 import {
@@ -9,7 +8,12 @@ import {
 import reducer from "..";
 
 describe("booking reducer", () => {
-  const initialState = {app:{ booking: {rooms: indexBy(prop("id"), defaultRooms)}}};
+  const initialState = reducer(undefined, { type: "null" });
+  const globalizeState = state => ({
+    app: {
+      booking: state
+    }
+  });
   const rooms = [
     { id: 1, priority: 1, selected: true, adults: "2", children: "1" },
     { id: 2, priority: 2, selected: true, adults: "1", children: "2" },
@@ -20,8 +24,53 @@ describe("booking reducer", () => {
   test("A new rooms value is set in state and can be selected", () => {
     const action = setRoomsValue(rooms);
     const newState = reducer(initialState, action);
-    const result = getRooms(newState);
+    const result = getRooms(globalizeState(newState));
 
     expect(result).toEqual(indexBy(prop("id"), rooms));
-  });  
+  });
+
+  test("roomValue changes after calling reducer", () => {
+    const action = roomValueChange(1, "adults", "2");
+    const newState = reducer(initialState, action);    
+    const result = getRoomById(globalizeState(newState), 1);
+
+    expect(result).toEqual({
+      id: 1,
+      priority: 1,
+      selected: true,
+      adults: "2",
+      children: "0"
+    });
+  });
+
+  test("roomSelected changes after calling reducer", () => {
+    const action = roomSelectionChange(2, "selected", true);
+    const newState = reducer(initialState, action);    
+    const result = getRoomById(globalizeState(newState), 2);
+
+    expect(result).toEqual({
+      id: 2,
+      priority: 2,
+      selected: true,
+      adults: "1",
+      children: "0"
+    });
+  });
+
+  test("when getRoomsId is called it gets the right Ids", () => {
+    const newRooms = [
+      {id: 5, priority: 5, selected: true, adults: "1", children: "0"},
+      {id: 6, priority: 6, selected: true, adults: "1", children: "0"},
+      {id: 7, priority: 7, selected: true, adults: "1", children: "0"},
+      {id: 8, priority: 8, selected: true, adults: "1", children: "0"}
+    ]
+
+    const action = setRoomsValue(newRooms);
+    const newState = reducer(initialState, action);
+    const result = getRoomsId(globalizeState(newState));
+
+    expect(result).toEqual(["5","6","7","8"]);
+  });
+
+
 });
